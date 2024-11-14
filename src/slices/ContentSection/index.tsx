1f5232dfc9a78ccf5e6baa1b846c5aade23d3a99
Lucas Espina <lespina@midasconsultores.com.ar>
2024-11-14 14:53:45 -0300

    chore: changing slices preview url

--- src/slices/ContentSection/index.tsx
+++ src/slices/ContentSection/index.tsx
@@ -17,16 +17,17 @@ const ContentSection = ({ slice }: ContentSectionProps): JSX.Element => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      style={{ width: "100vw", height: "100vh" }}
     >
       {isFilled.richText(slice.primary.title) && (
-        <div style={{ textAlign: "center" }}>
+        <div style={{ textAlign: "center", backgroundColor: "red" }}>
           <PrismicRichText field={slice.primary.title} />
         </div>
       )}
       {isFilled.image(slice.primary.simpson_image) && (
         <PrismicNextImage
           field={slice.primary.simpson_image}
-          style={{ width: "100vw", height: "100vh" }}
+          style={{ width: "100vw", height: "calc(100vh - 30px)" }}
         />
       )}
     </section>
